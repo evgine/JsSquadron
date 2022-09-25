125a6caae481166ceb0de57843c548e668d3df9e
yevginemnatsakanyan <evgine.v.mnatsakanyan@gmail.com>
2022-09-25 19:40:26 +0400

    Update caesarCipher.js

--- caesarCipher.js
+++ caesarCipher.js
@@ -1,1 +1,75 @@
+function encode(string, shift) {
+    let alphabet = "abcdefghijklmnopqrstuvwxyz";
+    let numbers = "0123456789";
+
+    string = String(string).toLowerCase().split("");
+    let result = [];
+
+    for (let letter of string) {
+        let indexOfLetter = alphabet.indexOf(letter);
+        let indexOfNumLetter = numbers.indexOf(letter);
+
+        let newIndexOfLetter = indexOfLetter + shift;
+        var newIndexOfNumLetter = indexOfNumLetter + shift;
+
+        if (letter === " ") {
+            result.push(letter);
+            continue;
+        }
+        if (indexOfLetter >= 0) {
+            if (newIndexOfLetter >= 26) {
+                result.push(alphabet[newIndexOfLetter - 25]);
+            } else {
+                result.push(alphabet[newIndexOfLetter]);
+            }
+        }
+        if (indexOfNumLetter >= 0) {
+            if (newIndexOfNumLetter >= 10) {
+                result.push(Number(numbers[newIndexOfNumLetter - 10]));
+            } else {
+                result.push(Number(numbers[newIndexOfNumLetter]));
+            }
+        }
+    }
+    return result.join("");
+}
+
+
+function decode(string, shift) {
+    let alphabet = "abcdefghijklmnopqrstuvwxyz";
+    let numbers = "0123456789";
+
+    string = String(string).toLowerCase().split("");
+    let result = [];
+
+    for (let letter of string) {
+        let indexOfLetter = alphabet.indexOf(letter);
+        let indexOfNumLetter = numbers.indexOf(letter);
+
+        let newIndexOfLetter = indexOfLetter - shift;
+        var newIndexOfNumLetter = indexOfNumLetter - shift;
+
+        if (letter === " ") {
+            result.push(letter);
+            continue;
+        }
+        if (indexOfLetter >= 0) {
+            if (newIndexOfLetter < 0) {
+                result.push(alphabet[newIndexOfLetter + 25]);
+            } else {
+                result.push(alphabet[newIndexOfLetter]);
+            }
+        }
+        if (indexOfNumLetter >= 0) {
+            if (newIndexOfNumLetter < 0) {
+                result.push(Number(numbers[newIndexOfNumLetter + 10]));
+            } else {
+                result.push(Number(numbers[newIndexOfNumLetter]));
+            }
+        }
+    }
+    return result.join("");
+}
+
+
 
